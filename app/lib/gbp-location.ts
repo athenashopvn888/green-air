c0552fe1d0c161396bc783394fcb5a0e702046fb
athenashopvn888 <athenashopvn888@gmail.com>
2026-06-19 13:07:17 +0700

    Add GBP local SEO landing pages with manual overrides

--- app/lib/gbp-location.ts
+++ app/lib/gbp-location.ts
@@ -24,8 +24,8 @@ export const gbpLocation = {
     "Accessories"
   ],
   menuUrl: "/",
-  directionsUrl: "https://greenaircannabis.com/",
-  mapEmbedUrl: "https://maps.google.com/maps?q=Green%20Air%20Cannabis%207060%20Airport%20Rd%2C%20Mississauga%2C%20ON%20L4T%202G8&t=&z=15&ie=UTF8&iwloc=&output=embed",
+  directionsUrl: "https://www.google.com/maps/search/?api=1&query=7060%20Airport%20Rd%2C%20Mississauga%2C%20ON%20L4T%202G8",
+  mapEmbedUrl: "https://maps.google.com/maps?q=7060%20Airport%20Rd%2C%20Mississauga%2C%20ON%20L4T%202G8&t=&z=15&ie=UTF8&iwloc=&output=embed",
   latitude: "43.6532",
   longitude: "-79.3832",
   hours: ["Open Daily: 10:00 AM - 02:00 AM"],
