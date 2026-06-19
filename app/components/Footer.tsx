4131f92d9d1c0a85d997a1f96e132b6a51322749
athenashopvn888 <athenashopvn888@gmail.com>
2026-06-19 12:04:13 +0700

    Add GBP local SEO landing pages with manual overrides

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -74,6 +74,7 @@ export default function Footer() {
               <Link href="/info/cheap-weed-york">Cheap Weed Mississauga</Link>
               <Link href="/info/native-cigarettes-york">Native Cigarettes</Link>
               <Link href="/info/weed-store-near-mississauga">Weed Store Near Mississauga</Link>
+              <Link href="/weed-dispensary-mississauga/">Green Air Cannabis Weed Dispensary in Mississauga</Link>
               <Link href="/contact">Contact Us</Link>
               <a
                 href="https://greenaircannabis.com/"
