69314f2c7ab10e2584a091168cdd1758e73d7fe5
athenashopvn888 <athenashopvn888@gmail.com>
2026-07-13 01:56:46 +0700

    Remove auto blog routes

--- app/components/GBPLandingPage.tsx
+++ app/components/GBPLandingPage.tsx
@@ -133,7 +133,7 @@ export function GBPLandingPage() {
         </p>
         <p className={styles.infoText}>
           For a fuller local overview, read the{" "}
-          <Link href="/blog/green-air-cannabis-mississauga-store-guide">Green Air Mississauga guide</Link>.
+          <Link href="/resources">Resources</Link>.
         </p>
       </section>
 
